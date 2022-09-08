b0ffd0ae72717160b2085cee4011f5e8c84af472
Musa Ibrahim <musaaj@gmail.com>
2022-09-08 11:15:44 -0700

    auto commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Write some text to the terminal
+* Description: Write formated text
+* Return: Int 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
