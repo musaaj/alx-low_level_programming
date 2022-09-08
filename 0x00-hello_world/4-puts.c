269a72a341ac35927a28af23ce8afae773c032ec
Musa Ibrahim <musaaj@gmail.com>
2022-09-08 09:05:10 -0700

    auto commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print a constant string to 
+ * main - print a constant string to the terminal
+* Description: Print text to the terminal
+* Return: Int 0 
 */
+
 int main( void )
 {
 	puts( "\"Programming is like building a multilingual puzzle" );
