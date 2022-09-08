6085764513747c5b4c66322052a9602d0794982e
Musa Ibrahim <musaaj@gmail.com>
2022-09-08 12:07:51 -0700

    auto commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	fprintf(stderr, "%s\n", "and that piece of art is useful\"\ 
+	fprintf(stderr, "%s\n", "and that piece of art is useful\" \ 
 	- Dora Korpar, 2015-10-19");
 	return (0);
 }
