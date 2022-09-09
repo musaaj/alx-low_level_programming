ec59b3c32e2e875cef31f11216f9ee93d2e194ac
Musa Ibrahim <musaaj@gmail.com>
2022-09-09 12:09:18 -0700

    auto commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabets
+*in lower case
+* Return: int 0
+*/
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 0; letter < 10; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar(10);
+	return (0);
+}
