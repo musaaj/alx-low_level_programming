6893ad73de32702d05c1d5aee76e25bec69a3b76
Musa Ibrahim <musaaj@gmail.com>
2022-09-09 12:51:23 -0700

    auto commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabets
+*in lower case
+* Return: int 0
+*/
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 48; letter < 58; letter++)
+	{
+		putchar(letter);
+		if (letter < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	return (0);
+}
