af28d495d7ea5097e53ad4bfa6ce61e97a445df2
Musa Ibrahim <=>
2022-09-13 22:28:46 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** print_alphabet_x10 - print english letters x10
+ * Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+	_putchar(10);
+}
