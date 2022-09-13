adb05569506e4cb43a80c49cd97fd87de6b1fd62
Musa Ibrahim <=>
2022-09-13 23:51:55 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - return abosolute value of a given number
+ * @n: int
+ * Return: int absolute value of 
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n)
+}
