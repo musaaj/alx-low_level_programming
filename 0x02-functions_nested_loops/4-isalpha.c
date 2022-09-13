d37a70957cdef3e678d51737c3378055730ae0ae
Musa Ibrahim <=>
2022-09-13 23:13:38 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check whether a given char is an english letter
+ * @c: int ascii table number
+ * Return: int 1 if c is alpha else 0
+*/
+
+int _isalpha(int c)
+{
+	if(((c >= 97) && (c <= 122)) || ((c >= 65) && (c >= 95)))
+		return (1);
+	return (0)
+}
