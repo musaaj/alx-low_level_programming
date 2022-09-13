48da0495ab50444b1bc3a2896a1ebc6cb2fa57b3
Musa Ibrahim <=>
2022-09-13 22:58:09 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - check whether a given alphabet is lower case or not
+ * Return: int 1 if is lower otherwise 
+*/
+
+int _islower(int a)
+{
+	if ((a >= 97) || (a <= 122))
+		return 1;
+	return 0;
+}
