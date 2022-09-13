57870cd91a45a4e839890580d984c0b1fb612fdb
Musa Ibrahim <=>
2022-09-13 21:40:33 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print lowercase alphabet
+ * Return: int 
+*/
+
+int main(void)
+{
+	char i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
