ed5ca890d577a20f3adbb0656dbe99dfe07cd6bb
Musa Ibrahim <=>
2022-09-13 23:38:06 +0100

    Auto Commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - print sign of a number
- * @c: int number
+ * @n: int number
  * Return: 1 if positive, 0 if 0, -1 if negative
 */
 
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('-');
+		_putchar('+');
 		return (1);
 	}
 	if (n == 0)
