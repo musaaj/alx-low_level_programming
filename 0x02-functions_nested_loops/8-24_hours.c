cce085885356dabe06ceea23d3745158cb424a54
Musa Ibrahim <=>
2022-09-14 06:40:48 +0100

    Auto Commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,24 +2,30 @@
 
 void jack_bauer(void)
 {
-	int h, m, hchar, mchar, z = 48;
+	int h, m, hchar, mchar, hchar1, mchar1, z = 48;
 
-	while ((h <= 23) && (m <= 59))
+	for (h = 0; h <= 23; h++)
 	{
-		hchar = h % 10;
-		mchar = m % 10;
-		_putchar(h + z);
-		_putchar(hchar + z);
-		_putchar(58);
-		_putchar(m + z);
-		_putchar(mchar + z);
-		_putchar(10);
-		m += 1;
-		if (m >= 59)
+		for(m = 0; m <= 59; m++)
 		{
-			h += 1;
-			m = 0;
+			hchar = h % 10;
+			mchar = m % 10;
+			hchar1 = h - hchar;
+			mchar1 = m - mchar;
+			if (hchar1 < 0)
+				hchar1 = hchar1 * (-1);
+			if (mchar1 < 0)
+				mchar1 = mchar1 * (-1);
+			if (hchar1 >= 10)
+				hchar1 = hchar1 / 10;
+			if (mchar1 >= 10)
+				mchar1 = mchar1 / 10;
+			_putchar(hchar1 + z);
+			_putchar(hchar + z);
+			_putchar(':');
+			_putchar(mchar1 + z);
+			_putchar(mchar + z);
+			_putchar(10);
 		}
-
 	}
 }
