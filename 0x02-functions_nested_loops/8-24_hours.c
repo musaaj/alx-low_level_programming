6da19c738cf9e48ed0192eb64795df874f8aac43
Musa Ibrahim <=>
2022-09-14 01:55:51 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+void jack_bauer(void);
+{
+	int h, m, hchar, mchar, z = 48;
+
+	while (h <= 23)
+	{
+		hchar = h % 10;
+		mchar = m % 10;
+		_putchar(h + z);
+		_putchar(hchar + z);
+		_putchar(58);
+		_putchar(m + z);
+		_putchar(mchar + z);
+		_putchar(10);
+		m += 1;
+		if (m > 59)
+		{
+			h += 1;
+			m = 0;
+		}
+
+	}
+}
