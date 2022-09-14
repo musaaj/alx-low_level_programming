11e76b8098e666113925dcf6a7587ecfbadf0c6e
Musa Ibrahim <=>
2022-09-14 07:49:56 +0100

    Auto Commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ * Return: void
+*/
+
+void times_table(void)
+{
+	int i, j, k, l, m, z = 48;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			
+			l = k % 10;
+			k = k - l;
+			_putchar(l + z);
+			while (k >= 10)
+			{
+				k = k / 10;
+				_putchar(k + z);
+			}
+			_putchar(k + z);
+			_putchar(44);
+			_putchar(32);
+		}
+		_putchar(10);
+	}
+}
