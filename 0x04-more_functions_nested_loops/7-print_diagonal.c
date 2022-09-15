7d3a37d1e9b40bf95e21a289060a1ac2e23213f5
Musa Ibrahim <=>
2022-09-15 21:52:33 +0100

    Auto Commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,8 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar(10);
 	for (i = 0; i < n; i++)
 	{
 		j = i;
@@ -20,5 +22,4 @@ void print_diagonal(int n)
 		_putchar(92);
 		_putchar(10);
 	}
-	_putchar(10);
 }
