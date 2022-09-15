96c322623c637a2730aeef92791470d28a946a2a
Musa Ibrahim <=>
2022-09-15 22:56:42 +0100

    Auto Commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * fizzbuzz - print fizzbuzz 
+ * fizzbuzz - print fizzbuzz
 */
 
 void fizzbuzz(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("%s", "FizzBuzz");
 		}
-		else if ((i % 3 == 0) && (i % 5 != 0)) 
+		else if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("%s", "Fizz");
 		}
