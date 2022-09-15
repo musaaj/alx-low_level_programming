77e79d9fb4407dcbfcec5091f4c66c0d98e4962c
Musa Ibrahim <=>
2022-09-15 09:44:17 +0100

    Auto Commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+/**
+ * main - check wether a given char is uppercase or not
+ * @c: int ascii char code
+ * Return: 1 if c is uppercase otherwise 
+*/
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+		return (1);
+	return (0);
+}
