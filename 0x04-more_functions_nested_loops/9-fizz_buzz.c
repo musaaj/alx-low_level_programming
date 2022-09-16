6f748af592ec811f232b5a1641df20d59af25495
Musa Ibrahim <=>
2022-09-16 12:30:40 +0100

    Auto Commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			printf("%s", "FizzBuzz");
 		}
-		else if ((i % 3 == 0) && (i % 5 != 0))
+		else if (i % 3 == 0)
 		{
 			printf("%s", "Fizz");
 		}
-		else if ((i % 3 != 0) && (i % 5 == 0))
+		else if (i % 5 == 0)
 		{
 			printf("%s", "Buzz");
 		}
