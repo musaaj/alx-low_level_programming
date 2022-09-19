d2e6cb8d5e6c2b3f0654bdeff2b279673e6af5b3
Musa Ibrahim <=>
2022-09-19 16:14:18 +0100

    Auto Commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_rev - print string in rev direction
+ * @s: string
+*/
+
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+
+	len -= 2;
+
+	while (len > 0)
+	{
+		putchar(s[len]);
+		len--;
+	}
+	putchar(10);
+}
