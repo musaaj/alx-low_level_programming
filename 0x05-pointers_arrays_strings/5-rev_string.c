4c78e0ba66df2e5857c377783e2cb8711a9e5148
= <=>
2022-09-19 17:41:26 +0100

    auto commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - print string in rev direction
+ * @s: string
+*/
+
+void rev_string(char *s)
+{
+	int len = _strlen(s);
+	static char *str;
+
+	len--;
+
+	while (len >= 0)
+	{
+		*(str + len) = s[len];
+		len--;
+	}
+	
+	s = str;
+}
