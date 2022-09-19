4de640b6adc801d1a019dcd60e2ddde5b5c69aa7
= <=>
2022-09-19 23:46:35 +0100

    auto commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy a string to another string
+ * @dest: char array string to copy to
+ * @src: char array source to copy from
+ * Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *itoc(int n);
-void _put(char *str);
+char *_strcpy(char *dest, char *src);
