746ef7952e78870e4375bbc1744a064192aa33b5
Musa Ibrahim <=>
2022-09-19 21:26:23 +0100

    Auto Commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - print the second halve of a string
+ * @str: string
+*/
+
+void puts_half(char *str)
+{
+	int len, halve;
+
+	len = _strlen(str);
+	halve = (len / 2);
+
+	if ((len % 2) == 1)
+		halve++;
+	while (str[halve] != '\0')
+	{
+		putchar(str[halve]);
+		halve;
+	}
+	putchar(10);
+}
