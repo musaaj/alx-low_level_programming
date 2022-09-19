94992fe1ed8e088add1f49265cf3c829156fba12
Musa Ibrahim <=>
2022-09-19 15:01:46 +0100

    Auto Commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two ints
+ * @a: int pointer
+ * @b: int pointer
+*/
+
+void swap_int(int *a, int *b)
+{
+	int p, q
+
+	p = *a;
+	q = *b;
+	*a = q;
+	*b = p;
+}
