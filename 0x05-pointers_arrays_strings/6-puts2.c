51afedc1bf2a98f0b93b85a947e26844f4ccc7bb
= <=>
2022-09-19 19:20:09 +0100

    auto commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts2 - print every other element of a string
+ * @str: string to be printed to the std
+*/
+
+void puts2(char *str)
+{
+	int len = _strlen(str);
+	int i = 0;
+
+	while (len >= 0)
+	{
+		putchar(str[i]);
+		len -= 2;
+		i += 2;
+	}
+}
