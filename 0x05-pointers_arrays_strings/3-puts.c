d62bf9da58d396a6ad7b4c247d4005d1312fd283
Musa Ibrahim <=>
2022-09-19 15:37:57 +0100

    Auto Commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print string to stdout
+ * @str: string
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+
+	if (i > 0)
+		putchar(10);
+}
