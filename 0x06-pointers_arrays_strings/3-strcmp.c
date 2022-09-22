d3d92ea07e723dc4a9f2d93d732796c884eb886c
= <=>
2022-09-22 12:34:56 +0100

    Auto Commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include "string.h"
+#include "strlen.c"
+
+/**
+* _strcmp - compare 2 string
+* @s1: string
+* @s2: string
+* Return: int
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int len1 = _strlen(s1), len2 = _strlen(s2)
+	,dff = len1 - len2, i = 0, result = 0;
+
+	if (dff < 0)
+		dff *= -1;
+	len1 += dff;
+	for (i = 0; i < len1; i++)
+	{
+		if (s1[i] - s2[i] != 0)
+		{
+			result = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (result);
+}
+
