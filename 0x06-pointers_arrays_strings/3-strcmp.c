cee54fc0712829d12df5c8d0edab0aa935442f1a
= <=>
2022-09-22 12:36:59 +0100

    Auto Commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,8 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = _strlen(s1), len2 = _strlen(s2)
-	,dff = len1 - len2, i = 0, result = 0;
+	int len1 = _strlen(s1), len2 = _strlen(s2);
+	int dff = len1 - len2, i = 0, result = 0;
 
 	if (dff < 0)
 		dff *= -1;
