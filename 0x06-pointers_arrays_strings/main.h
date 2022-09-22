3daa1f9baba8ffef7df6d4a7ccb68dcca773bdde
= <=>
2022-09-22 22:08:17 +0100

    Auto Commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,3 +11,5 @@ char *cap_string(char *);
 int is_seperator(char);
 char *leet(char *);
 char *replace(char *dest, char *chk, char rplc);
+
+#endif
