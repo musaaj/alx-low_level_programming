fbcac62e95c2c56cc7d311561536b9253da63a3c
= <=>
2022-09-22 13:47:09 +0100

    Auto Commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* reverse_array - compare 2 string
+* @a: int array
+* @n: int length of a
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, tmp[100];
+
+	n--;
+	for (i = n; i >= 0; i--)
+	{
+		tmp[i] = a[n-i];
+	}
+	for (i = 0; i <= n; i++)
+	{
+		a[i] = tmp[i];
+	}
+
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *);
 int _strcmp(char *, char *);
+void reverse_array(int *, int);
