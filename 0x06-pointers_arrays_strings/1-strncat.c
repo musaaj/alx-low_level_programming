4259f341f2d36d467a40ae65711e7102d0fd1223
= <=>
2022-09-22 07:19:24 +0100

    Correct strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "string.h"
+#include "../0x05-pointers_arrays_strings/2-strlen.c"
+/**
+* _strncat - concate 2 string
+* @dest: string
+* @src: string
+* @n: number of characters to append to dest
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int len1 = _strlen(dest);
+	int nlen = len1 + n;
+
+	while (src[i] != EOS)
+	{
+		if (i >= nlen)
+			break;
+		dest[len1 + i] = src[i];
+		i++;
+	}
+	nlen++;
+	dest[nlen] = EOS;
+	return (dest);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,2 @@
-char *_strcat(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
