9ec4c6467c036689d185ddda29ef9a98c7fd4036
= <=>
2022-09-26 21:21:40 +0100

    Auto Commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * _strchr - find the first occurence of a char in a string
+ * @s: string to search through
+ * @c: char to look for
+ * Return: pointer to first occurence of c if found else return NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s = c)
+			return s;
+		s++;
+	}
+	return (0);
+}
