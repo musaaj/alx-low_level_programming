031ca8ecc605ea22cda752623bf32ec9e1457181
Musa Ibrahim <=>
2022-09-27 22:57:25 +0100

    Auto Commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,10 +6,12 @@
 #include "1-memcpy.c"
 #include "2-strchr.c"
 #include "3-strspn.c"
+#include "4-strpbrk.c"
 
 char *_memset(char *, int, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
