c0df1cf27b3e3bc75c43b5a256fcd4e5bfc60f05
= <=>
2022-09-27 18:52:16 +0100

    Auto Commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,31 +8,13 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	int len = _strlen(s);
 
-	while (i <= len)
+	while (s[i] != '\0')
 	{
-		if (*(s + i) == c)
+		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
 	return (NULL);
 }
 
-/**
- * _strlen - finds length of a given string
- * @s: string
- * Return: int length of s
-*/
-
-int _strlen(char *s)
-{
-	int len = 0, i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-		len++;
-	}
-	return (len);
-}
