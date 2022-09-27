7f052472f65e72de4aa9e0246e9b9d6e1177e39f
= <=>
2022-09-27 17:31:38 +0100

    Auto Commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define NULL (*(void) 0)
-
+#include <stdio.h>
 #include "0-memset.c"
 #include "1-memcpy.c"
-#include "2-strchr.c"
 #include "strlen.c"
+#include "2-strchr.c"
 
-char *_memset(char *, int, unsigned int n);
+char *_memset(char *, int, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strlen(char *);
