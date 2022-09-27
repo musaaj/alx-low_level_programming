c068bb689aa29e94a1b6d8275b67e72a24806bed
Musa Ibrahim <=>
2022-09-27 22:58:55 +0100

    Auto Commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,18 @@
-/**
- * _strchr - find the first occurence of a char in a string
- * @s: string to search through
- * @c: char to look for
- * Return: pointer to first occurence of c if found else return NULL
-*/
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
-	}
-	if (c == '\0')
-		return (s + i);
-	return (NULL);
-}
-
+#include "main.h" 
+ /** 
+  * _strchr - locates a character in a string, 
+  * @s: string. 
+  * @c: character. 
+  * Return: the pointer to the first occurrence of the character c. 
+  */ 
+ char *_strchr(char *s, char c) 
+ { 
+         unsigned int i = 0; 
+  
+         for (; *(s + i) != '\0'; i++) 
+                 if (*(s + i) == c) 
+                         return (s + i); 
+         if (*(s + i) == c) 
+                 return (s + i); 
+         return ('\0'); 
+ }
