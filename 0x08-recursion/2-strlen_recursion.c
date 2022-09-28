e367a68134c5301d44a1abfc0df9c7a76cea50c2
= <=>
2022-09-28 21:21:33 +0100

    Auto Commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * _strlen_recursion - find the length of a given str
+ * @s: str 
+ * Return: int length of @str
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	
+	return (1 + _strlen_recursion(s++);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,4 +3,6 @@
 
 unsigned int _putchar(char c);
 void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
+
 #endif
