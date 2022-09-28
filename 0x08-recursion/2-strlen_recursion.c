93b93a12d0dfe93c7e43db9b62f9a9732fd66b86
= <=>
2022-09-28 21:28:58 +0100

    Auto Commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 /**
  * _strlen_recursion - find the length of a given str
- * @s: str 
+ * @s: str
  * Return: int length of @str
 */
 
@@ -8,6 +8,5 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	
 	return (1 + _strlen_recursion(s + 1));
 }
