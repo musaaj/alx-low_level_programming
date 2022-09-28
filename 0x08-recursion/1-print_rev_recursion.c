8d64a76b62637a38aaabe79954017e4f38f68ec6
= <=>
2022-09-28 22:01:49 +0100

    Auto Commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "2-strle_recursion.c"
+#include "_putchar.c"
+
+/**
+ * _print_rev_recursion - print string in reverse order
+ * @s: str
+*/
+void _print_rev_recursion(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	_print(s, len)
+}
+
+/**
+ * _print - print string in rev order
+ * @s: str
+ * @len: int length of @s
+*/
+void _print(char *s, int len)
+{
+	if (len < 0)
+	{
+		_putchar(10);
+		return;
+	}
+	_print(s, len - 1);
+}
