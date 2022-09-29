747e3a9e4ec1f119523860e1572e529b179975cb
= <=>
2022-09-29 20:18:11 +0100

    Auto Commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,11 +2,11 @@ int _sqrt_recursion(int n)
 {
 	int hv = n / 2;
 
-	if ((*_sqrt_recursion) * (*_sqrt_recursion) == n)
+	if (_sqrt_recursion(n) * _sqrt_recursion(n) == n)
 	{
 		return (_sqrt_recursion(n));
 	}
-	if (_sqrt_recursion == hv)
+	if (_sqrt_recursion(n) == hv)
 	{
 		return (-1);
 	}
