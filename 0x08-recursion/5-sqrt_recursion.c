51f2bd1e0dfb64944e02e7eb82b5f2be08c8e6d7
= <=>
2022-09-29 21:07:01 +0100

    Auto Commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,33 @@
 #include "main.h"
+
+/**
+ * _sqrt_recursion - find natural sqrt of a number
+ * @n: int
+ * Return: natural sqrt of @n if it has otherwise 1
+*/
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(0, n));
 
 }
 
+/**
+ * _sqrt - check whether a given number has a natural sqrt
+ * @i: int
+ * @n: int
+ * Return: sqrt of @n
+*/
 int _sqrt(int i, int n)
 {
 	int hv = n / 2;
 
+	if (n == 1)
+		return (1);
+	if (n == 0)
+		return (0);
 	if (i * i == n)
 		return (i);
 	if (i >= hv)
 		return (-1);
-	i++;
-	return (_sqrt(i, n));
+	return (_sqrt(i + 1, n));
 }
