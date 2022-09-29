a75050e77ea2f86e16d11c4d30be77d9bcb1d4b6
= <=>
2022-09-29 09:47:31 +0100

    Auto Commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,14 @@
+/**
+ * _pow_recursion - raise number to a power
+ * @x: base
+ * @y: index
+ * Return: @x raised to power of @y
+*/
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -(1);
+		return (-1);
 	if (y == 0)
 		return (1);
 	if (y == 1)
