b8f5886018b7368be10249a6d4ebd53e56ea436d
Musa Ibrahim <=>
2022-10-03 21:16:20 +0100

    Auto Commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,12 +1,19 @@
 #include <stdlib.h>
 
+/**
+ * create_array - create an array prepopulated with a giving char
+ * @size: int
+ * @c: char
+ * Return: char array populated with @c
+ */
+
 char *create_array(unsigned int size, char c)
 {
-	static char *chr = NULL;
+	char *chr = NULL;
 	int i;
 
 	chr = (char *) malloc(size);
-	if ( chr == NULL)
+	if (chr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
