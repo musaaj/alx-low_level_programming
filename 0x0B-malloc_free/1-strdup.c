3fb1e6a2b3fceba9cfbc66f21642ddd4ed556980
Musa Ibrahim <=>
2022-10-03 23:06:27 +0100

    Auto Commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicate a string
+ * @str: string
+ * Return: pointer to first element of duplicate of @str
+ */
+char *_strdup(char *str)
+{
+	char *ptr = NULL;
+	unsigned int i = 0, len = strlen(str);
+
+	ptr = (char *) malloc(len);
+	if (str == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
