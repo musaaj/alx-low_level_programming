04efdfc9d2da36f523769750e316440ba1d2f8b5
= <=>
2022-10-04 13:26:48 +0100

    Auto Commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,11 +2,6 @@
 #define MAIN_H
 #define NULL ((void *) 0)
 
-#include "0-memset.c"
-#include "1-memcpy.c"
-#include "2-strchr.c"
-#include "3-strspn.c"
-#include "4-strpbrk.c"
 
 char *_memset(char *, int, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
