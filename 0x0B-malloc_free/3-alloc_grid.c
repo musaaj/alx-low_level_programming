c4e929af6788453cc7f6b04d74cb4879ddbe6f40
= <=>
2022-10-04 09:17:53 +0100

    Auto Commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 		_height[i] = malloc(sizeof(int) * width);
 		if (_height[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j <= i; j++)
 			{
 				free(_height[j]);
 			}
