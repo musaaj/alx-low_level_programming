ac68f812b19d8dcc6f89f94a9ef98a1068f8495a
= <=>
2022-10-04 13:20:43 +0100

    Auto Commit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,23 @@
+#include "2-str_concat.c"
+
+/**
+* argstostr - turn program args to a str
+* @ac: int length of program args
+* @av: string program args
+* Return: string
+*/
+char *argstostr(int ac, char **av)
+{
+	char *str = NULL;
+	int i;
+
+	if ((ac == 0) || (av == NULL))
+		return (NULL);
+	str = str_concat("","");
+	for (i = 0; i < ac; i++)
+	{
+		str = str_concat(str, av[i]);
+		str = str_concat(str, "\n");
+	}
+	return (str);
+}
