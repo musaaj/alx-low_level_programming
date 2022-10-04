5bb37dec60ece2502694701fbda063138399d933
= <=>
2022-10-04 01:43:52 +0100

    Auto Commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,5 +14,7 @@ int **alloc_grid(int width, int height)
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
 	_width = malloc(sizeof(int) * width * height);
+	if (_width == NULL)
+		return (NULL);
 	return (_width);
 }
