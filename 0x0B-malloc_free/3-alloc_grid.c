02afb5c06dfd8a5b009f2008fdc8879b7be583aa
Musa Ibrahim <=>
2022-10-04 03:05:21 +0100

    Auto Commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,9 +10,13 @@ int **alloc_grid(int width, int height)
 	int i, j,  **_width;
 
 	_width = (int **) malloc(sizeof(int *) * width);
+	if (_width == NULL)
+		return (NULL)
 	for (i = 0; i < width; i++)
 	{
 		_width[i] = (int *) malloc(sizeof(int) * height);
+		if (_width[i] = NULL)
+			return (NULL);
 		for (j = 0; j < height; j++)
 		{
 			_width[i][j] = 0;
