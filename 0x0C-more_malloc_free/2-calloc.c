de0374670f8e17193b84c14e72acb43ff883efff
Musa Ibrahim <=>
2022-10-06 10:01:08 +0100

    Auto Commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	char *ptr;
+
+	if ((nmemb == 0) || (size == 0))
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= (nmemb * size); i++)
+	{
+		*(ptr + i) = 0;
+	}
+	return (ptr);
+}
