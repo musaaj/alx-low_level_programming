65b7c6b5731721a7b5b7287fe1346155a89b960f
= <=>
2022-10-10 14:14:36 +0100

    Auto Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - hold info about dogs
+* @age: age of a particular dog
+* @name: name of a particular dog
+* @owner: name of a particular dog owner
+*/
+struct dog
+{
+	float age;
+	char *name;
+	char *owner;
+}
+#endif
