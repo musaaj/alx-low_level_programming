1b5bbdc526c17600ee6d441037b77a7acd6cd787
= <=>
2022-10-10 15:55:28 +0100

    Auto Commit

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    my_dog.name = "Poppy";
+    my_dog.age = (int) NULL;
+    my_dog.owner = NULL;
+    print_dog(&my_dog);
+    return (0);
+}
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,5 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if (d->name == NULL)
+		d->name = "(nil)"
 	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
