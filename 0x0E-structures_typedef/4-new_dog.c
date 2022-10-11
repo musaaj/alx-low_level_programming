8345398222a52e87cedb8de0a61a29010e8bb659
Musa Ibrahim <=>
2022-10-11 22:49:43 +0100

    Auto Commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,40 +11,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *nm, *own;
 
-	nm = malloc(strlen(name) + 1);
-	if (nm == NULL)
-		return (NULL);
-	own = malloc(strlen(owner) + 1);
-	if (own == NULL)
-		return (NULL);
-	dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog) + strlen(name) + strlen(owner));
 	if (dog == NULL)
-	{
-		free(nm);
-		free(own);
 		return (NULL);
-	}
-	dog->name = malloc(strlen(nm));
-	if (dog->name == NULL)
-	{
-		free(nm);
-		free(own);
-		free(dog);
-		return(NULL);
-	}
-	dog->owner = malloc(strlen(own));
-	if (dog->owner == NULL)
-	{
-		free(nm);
-		free(own);
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	strcpy(dog->name, nm);
-	strcpy(dog->owner, own);
+	dog->name = name;
+	dog->owner =  owner;
 	dog->age = age;
 	return (dog);
 }
