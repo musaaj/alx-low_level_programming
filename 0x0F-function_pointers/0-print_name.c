bd6f570aceb3712ff4ab9b953ffbb43f34b5e4bc
= <=>
2022-10-12 21:25:38 +0100

    Auto Commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointer.h"
+/**
+* print_name - print a given name
+* @a: string
+* @f: void function pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	(f)(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef POINTER_H
+#define POINTER_H
+void print_name(char *name, void (*f)(char *));
+void _puts(char *s);
+void (*f)(char *);
+#endif
