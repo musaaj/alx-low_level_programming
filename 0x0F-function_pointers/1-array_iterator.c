174ee9965dfe81ad8738518fc27e16331e9f5414
= <=>
2022-10-12 22:33:37 +0100

    Auto Commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* array_iterator - iterate over a giving array
+* @array: int
+* @size: int length of @array
+* @action: func to call for each element
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action == NULL)
+		return;
+	if (size == 0)
+		return;
+	while (i < size)
+	{
+		(*action)(array[i])
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
 #ifndef POINTER_H
 #define POINTER_H
 void print_name(char *name, void (*f)(char *));
-void _puts(char *s);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
