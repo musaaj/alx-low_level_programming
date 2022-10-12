c16ed7ee8f6772b278a4bdd1ff624512c10afe4e
= <=>
2022-10-12 21:54:21 +0100

    Auto Commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 /**
 * print_name - print a given name
-* @a: string
+* @name: string
 * @f: void function pointer
 */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(*f)(name);
 }
