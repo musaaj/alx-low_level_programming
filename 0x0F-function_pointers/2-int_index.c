f251f0ee409120eedc77409ef1b3058f6a1719c1
= <=>
2022-10-12 23:28:19 +0100

    Auto Commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+* index_array - find first index of occurence of an integer in an
+* integer array
+* @array: int array
+* @size: number of element of @array
+* @cmp: function to compare
+* Return: int;
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+
+}
