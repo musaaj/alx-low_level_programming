6079ed4664553de4a6f2e5bccd1a61fc0197c7fa
= <=>
2022-10-13 09:09:29 +0100

    Auto Commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,6 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (size <= 0)
 		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]))
