05eed284130dd914b2984f92c239384e47676ba0
= <=>
2022-10-14 00:09:53 +0100

    Auto Commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,6 +4,12 @@
 #include <string.h>
 #include "3-calc.h"
 
+/**
+ * main - main entry of our program
+ * @argc: argument count
+ * @argv: list of cli arguments
+ * Return: zero on success
+ */
 int main(int argc, char *argv[])
 {
 	int a, b;
@@ -18,7 +24,12 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	op = argv[2];
-	
+
+	if(((*op == '/') && b == 0) || ((*op == '%') && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	f = (*get_op_func)(op);
 	if (f == NULL)
 	{
