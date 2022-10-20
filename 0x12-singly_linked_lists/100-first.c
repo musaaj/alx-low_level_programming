093fc2434cce44f896deae785de9dfa2f04cb45f
= <=>
2022-10-21 00:28:37 +0100

    Auto Commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,8 @@
  */
 void construct(void)
 {
-	printf("You're beat! and yet, you must allow, \
-	\nI bore my house upon my back!\n");
+	char *s = "You're beat! and yet, you must allow,";
+	char *s1 = "\nI bore my house upon my back!\n";
+
+	printf("%s%s", s, s1);
 }
