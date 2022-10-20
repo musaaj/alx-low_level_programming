d4a9879651ce9fd112adb0d5a04cec41e12d2b41
= <=>
2022-10-21 00:20:50 +0100

    Auto Commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * construct - do some setup
+ */
+void construct(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
