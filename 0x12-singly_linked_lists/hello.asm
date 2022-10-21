6d6751097d7942f48711e55bb93af295c1b13c1c
= <=>
2022-10-21 01:25:20 +0100

    Auto Commit

---
+++ 0x12-singly_linked_lists/hello.asm
@@ -0,0 +1,15 @@
+SECTION .data
+	msg db "Hello World",10,0
+
+SECTION .text
+
+_start:
+	global _start
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 14
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
