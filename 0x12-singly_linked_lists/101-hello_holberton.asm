0105a1443013204342d414eb9f72d689b2609fc4
= <=>
2022-10-21 11:20:56 +0100

    Auto Commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
-SECTION .data
+section .data
 	msg db "Hello, Holberton",10,0
 
-SECTION .text
+section .text
 
-_start:
-	global _start
+global main
+main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
-	mov rdx, 16
+	mov rdx, 17
 	syscall
 	mov rax, 60
 	mov rdi, 0
