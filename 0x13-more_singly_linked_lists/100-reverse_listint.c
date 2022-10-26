18103a7f967f0ab57cb48faccc763190a96c5088
= <=>
2022-10-26 17:01:43 +0100

    Auto Commit

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 /**
+ * reverse_listint - revserse order of elements of a lsitint object
+ * @head: first element of a listint object
+ * Return: address of the first element of the listint object reversed
 */
 listint_t *reverse_listint(listint_t **head)
 {
