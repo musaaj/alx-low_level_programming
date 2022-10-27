ce943925be655c7576cc15dbee43bcfaf316c9e9
= <=>
2022-10-27 23:24:44 +0100

    Auto Commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,18 @@
 #include "main.h"
+#include <unistd.h>
 /**
+ * print_binary - print a number in binary form
+ * @n: umsigned int
  */
 void print_binary(unsigned long int n)
 {
+	char c;
+
+	while (n)
+	{
+		c = n & 1;
+		c = c + 48;
+		write(1, &c, 1);
+		n >>= 1;
+	}
 }
