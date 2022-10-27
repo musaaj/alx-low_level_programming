9a50b82c84f8cc366e44f63cab39bfaa86d3222c
= <=>
2022-10-28 00:16:17 +0100

    Auto Commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,22 @@
 void print_binary(unsigned long int n)
 {
 	char c = 48;
-	
+	char s[32];
+	int i = 31, count = 0;
+
+	s[i] = '\0';
 	if (!n)
 		write(1, &c, 1);
 	while (n)
 	{
-		c = n & 1;
-		c = c + 48;
-		write(1, &c, 1);
+		i--;
+		if (n & (n & 1))
+			c = 49;
+		else
+			c = 48;
+		s[i] = c;
 		n >>= 1;
+		count++;
 	}
+	write(1, &s[i], count);
 }
