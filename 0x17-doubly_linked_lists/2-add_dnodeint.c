fea79b8b2cddabad3b59428c8fd925e63acb7cbc
= <=>
2022-12-11 19:04:03 +0100

    Auto Commit

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - add a new node at the beginning of a
+* dlistint_t object
+* @head: address of address of first element in a
+* dlistint_t object
+* @n: value of the node to be added
+* Return: dlistint_t newly added node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
