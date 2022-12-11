89a2c7945b0da791afb293a66633119d31cf571a
= <=>
2022-12-11 19:15:07 +0100

    Auto Commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,13 +15,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (!head)
 		return (NULL);
-	if (!(*head))
-		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
+	if (*head)
+		(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
