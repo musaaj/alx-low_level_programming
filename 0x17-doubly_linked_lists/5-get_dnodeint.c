a92d21deeda38760dab0792bd03bc2ab0416deb5
Musa Ibrahim <=>
2022-12-11 21:03:21 +0100

    Auto Commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get element of a dlistint_t object by index
+ * @head: head node of the dlistint_t object
+ * @index: index of the node to be gotten
+ * Return: not at @index if it exist else NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned i;
+
+	if (!head)
+		return (NULL);
+	tmp = head;
+	i = 0;
+	while (tmp)
+	{
+		if ( i == n)
+			return (tmp);
+		tmp = tmp->next;
+		i++;
+	}
+	return (NULL)
+}
