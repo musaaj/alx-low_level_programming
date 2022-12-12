61fc10d4e8a59acfb5ba13f25bff23662e6b2243
= <=>
2022-12-12 17:35:59 +0100

    Auto Commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,43 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * delete_dnodeint_at_index - delete an element of a dlistint_t object
+ * @head: pointer to the first node of a dlistint_t object
+ * @index: index of the element to be removed
+ * Return: 1 on success else 0
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	int i = 0;
+	dlistint_t *tmp, *dtmp;
+
+	if (index == 0)
+	{
+		if (*head)
+		{
+			tmp = *head;
+			*head = tmp->next;
+			if (*head)
+				(*head)->prev = NULL;
+			free(tmp);
+			return (1);
+		}
+	}
+	dtmp = *head;
+	while (dtmp)
+	{
+		if (i == index)
+		{
+			tmp = *dtmp;
+			dtmp->next = tmp->next;
+			if(tmp->next)
+				(tmp->next)->prev = dtmp;
+			free(tmp);
+			return (1)
+		}
+		i++;
+		dtmp = dtmp->next;
+	}
+	return (0);
 }
