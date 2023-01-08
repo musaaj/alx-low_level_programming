5c398d295f8f7b70282c0a84951fdea813acfcbd
Musa Ibrahim <musaaj@gmail.com>
2023-01-08 17:45:22 +0100

    Auto Commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,14 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!size)
 		return (NULL);
-	hash_table = malloc(sizeof(hash_table_t *));
+	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 		return (NULL);
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(hash_table->array))
 		return (NULL);
 	hash_table->size = size;
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
 	return (hash_table);
 }
