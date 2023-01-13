38d0c6930f2236e93636205e7925702186257014
Musa Ibrahim <=>
2023-01-13 22:14:27 +0100

    Auto Commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,5 +12,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 		return (0);
 	if (!size)
 		return (0);
-	return ((hash_djb2(key) ^ size) % size);
+	return (hash_djb2(key) % size);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[idx] = hash_node;
 		return (1);
 	}
-	if (hash_node_insert(ht->array[idx], hash_node))
+	if (hash_node_insert(&(ht->array[idx]), hash_node))
 		return (1);
 	return (0);
 }
@@ -43,13 +43,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * @hash_node: new node to be inserted
  * Return: 1 on success else 0
  */
-int hash_node_insert(hash_node_t *head, hash_node_t *hash_node)
+int hash_node_insert(hash_node_t **head, hash_node_t *hash_node)
 {
 	if (!head)
 		return (0);
 	if (!hash_node)
 		return (0);
-	hash_node->next = head;
-	head = hash_node;
+	hash_node->next = *head;
+	*head = hash_node;
 	return (1);
 }
